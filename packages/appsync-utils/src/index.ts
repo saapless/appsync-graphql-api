14465d013b33170ea8c59f8abc143c086c4464b7
Silviu Glavan <silviu@gigs.ro>
2025-01-17 08:33:33 +0200

    fix(transformer): code generators

--- packages/appsync-utils/src/index.ts
+++ packages/appsync-utils/src/index.ts
@@ -1,1 +1,1 @@
-export {};
+export { isAuthorized } from "./isAuthorized";
---
+++ packages/appsync-utils/src/isAuthorized.ts
@@ -0,0 +1,6 @@
+import { Context } from "@aws-appsync/utils";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function isAuthorized(ctx: Context, rules: unknown): boolean {
+  return true;
+}
