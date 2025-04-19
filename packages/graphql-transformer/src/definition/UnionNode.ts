5614e7f3a4e3380618ce32d67750616d4b28e8b3
Silviu Glavan <silviu@gigs.ro>
2025-04-19 19:30:51 +0300

    chore(transformer): added UnionNode tests

--- packages/graphql-transformer/src/definition/UnionNode.ts
+++ packages/graphql-transformer/src/definition/UnionNode.ts
@@ -1,5 +1,4 @@
 import {
-  ConstDirectiveNode,
   Kind,
   UnionTypeDefinitionNode,
   UnionTypeExtensionNode,
@@ -7,29 +6,22 @@ import {
 } from "graphql";
 import { DirectiveNode } from "./DirectiveNode";
 import { NamedTypeNode } from "./TypeNode";
+import { WithDirectivesNode } from "./WithDirectivesNode";
 
-export class UnionNode {
+export class UnionNode extends WithDirectivesNode {
   kind: Kind.UNION_TYPE_DEFINITION = Kind.UNION_TYPE_DEFINITION;
   name: string;
   types?: NamedTypeNode[] | undefined;
-  directives?: DirectiveNode[] | undefined;
 
-  constructor(
-    name: string,
-    types?: NamedTypeNode[] | null,
-    directives?: DirectiveNode[] | undefined
-  ) {
+  constructor(name: string, types?: NamedTypeNode[], directives?: DirectiveNode[] | undefined) {
+    super(name, directives);
+
     this.name = name;
     this.types = types ?? undefined;
-    this.directives = directives;
   }
 
   public hasType(type: string) {
     return this.types?.some((node) => node.name === type) ?? false;
-  }
-
-  public getType(type: string) {
-    return this.types?.find((node) => node.name === type);
   }
 
   public addType(type: string | NamedTypeNode | INamedTypeNode) {
@@ -52,32 +44,6 @@ export class UnionNode {
 
   public removeType(type: string) {
     this.types = this.types?.filter((node) => node.name !== type);
-    return this;
-  }
-
-  public hasDirective(name: string) {
-    return this.directives?.some((node) => node.name === name) ?? false;
-  }
-
-  public getDirective(name: string) {
-    return this.directives?.find((node) => node.name === name);
-  }
-
-  public addDirective(directive: string | DirectiveNode | ConstDirectiveNode) {
-    const node =
-      directive instanceof DirectiveNode
-        ? directive
-        : typeof directive === "string"
-          ? DirectiveNode.create(directive)
-          : DirectiveNode.fromDefinition(directive);
-
-    this.directives = this.directives ?? [];
-    this.directives.push(node);
-    return this;
-  }
-
-  public removeDirective(name: string) {
-    this.directives = this.directives?.filter((node) => node.name !== name);
     return this;
   }
 
@@ -111,17 +77,22 @@ export class UnionNode {
     };
   }
 
-  static create(name: string, types: (NamedTypeNode | string)[] = []): UnionNode {
+  static create(
+    name: string,
+    types?: (NamedTypeNode | string)[],
+    directives?: DirectiveNode[]
+  ): UnionNode {
     return new UnionNode(
       name,
-      types.map((type) => (type instanceof NamedTypeNode ? type : NamedTypeNode.create(type)))
+      types?.map((type) => (type instanceof NamedTypeNode ? type : NamedTypeNode.create(type))),
+      directives
     );
   }
 
   static fromDefinition(definition: UnionTypeDefinitionNode) {
     return new UnionNode(
       definition.name.value,
-      definition.types?.map((node) => NamedTypeNode.create(node.name.value)) ?? null,
+      definition.types?.map((node) => NamedTypeNode.create(node.name.value)),
       definition.directives?.map((node) => DirectiveNode.fromDefinition(node))
     );
   }
