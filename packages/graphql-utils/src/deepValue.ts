8f371300640ae404534db6655277c8f80d31163d
Silviu Glavan <silviu@gigs.ro>
2025-04-20 13:06:14 +0300

    fix(graphql-utils): fixes & tests

--- packages/graphql-utils/src/deepValue.ts
+++ packages/graphql-utils/src/deepValue.ts
@@ -1,4 +1,4 @@
-export function getValue(obj: object, path: string) {
+export function getValue(obj: unknown, path: string) {
   return path.split(".").reduce<typeof obj | undefined>((acc, key) => {
     if (acc && typeof acc === "object" && Object.hasOwn(acc, key)) {
       return acc[key as keyof typeof acc];
